6355cc2cba85f2e1a9e5f72018357f5880cf5ffb
douryne <douryne.1st@gmail.com>
2022-12-24 15:34:12 +0400

    ContentWrapper component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup>
+  import ContentWrapper from './components/ContentWrapper.vue';
 </script>
 
 <template>
+  <div class="wrapper">
+    <header>
+      <h1>Форма</h1>
+    </header>
+    <main>
+      <ContentWrapper />
+    </main>
+  </div>
 </template>
 
 <style scoped>
+  .wrapper {
+    --app-padding-x: 20px;
+    
+    max-width: 570px;
+    border-radius: 10px;
+    padding: 10px var(--app-padding-x) 36px var(--app-padding-x);
+    background-color: #fff;
+  }
+  header {
+    text-align: center;
+  }
+  h1 {
+    font: var(--default-font);
+  }
 </style>
---
+++ src/components/ContentWrapper.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+    <div class="wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .wrapper {
+        position: relative;
+    }
+    .wrapper::before {
+        --width: 8px;
+        
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: calc((var(--width) + var(--app-padding-x)) * -1);
+        width: var(--width);
+        background-color: #D9D9D9;
+        border-radius: 10px;
+    }
+</style>
